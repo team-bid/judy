854974cd52596bfb4aea2026c3c97f6fd925271f
Darren Wee <darren.wee@u.nus.edu>
2017-04-18 23:29:26 +0800

    add __repr__ methods

--- judy_objects.py
+++ judy_objects.py
@@ -3,16 +3,21 @@ class Node:
     bitmap = ''
     is_item = False
 
-    def __init__(self, is_item, bitmap, parent, left_child, right_child):
+    def __init__(self, is_item, bitmap, parent=None, left_child=None, right_child=None):
         self.parent = parent
         self.bitmap = bitmap
         self.is_item = is_item
         self.left_child = left_child
         self.right_child = right_child
-        TreeNode.node_count += 1
+        Node.node_count += 1
 
     def get_number_of_nodes(self):
-        return TreeNode.node_count
+        return Node.node_count
+
+    def __repr__(self):
+        if self.is_item:
+            return '[I: %s]' % self.bitmap
+        return '[Q: %s]' % self.bitmap
 
 class Item:
     probability = 0.0
@@ -21,6 +26,9 @@ class Item:
 
     def __init__(self, name, probability, bitmap):
         self.name = name
-        self.probability = probability
+        self.probability = float(probability)
         self.bitmap = bitmap
         return
+
+    def __repr__(self):
+        return '<%s, %.2f: %s>' % (self.name, self.probability, self.bitmap)
